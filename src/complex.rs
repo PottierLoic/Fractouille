75219a583cf06c58c860858dbb6673f9a3ec648d
Loïc Pottier <loic.pottier@live.fr>
2025-08-04 09:50:11 -0700

    feat: add `Complex` struct

---
+++ src/complex.rs
@@ -0,0 +1,49 @@
+#[derive(Clone, Copy, Debug)]
+pub struct Complex {
+  pub re: f64,
+  pub im: f64,
+}
+
+impl Complex {
+  pub fn new(re: f64, im: f64) -> Self {
+    Complex { re, im }
+  }
+
+  pub fn abs_sq(&self) -> f64 {
+    self.re * self.re + self.im * self.im
+  }
+
+  pub fn add(self, other: Complex) -> Complex {
+    Complex {
+      re: self.re + other.re,
+      im: self.im + other.im,
+    }
+  }
+
+  pub fn square(self) -> Complex {
+    Complex {
+      re: self.re * self.re - self.im * self.im,
+      im: 2.0 * self.re * self.im,
+    }
+  }
+
+  pub fn abs(self) -> Complex {
+    Complex {
+      re: self.re.abs(),
+      im: self.im.abs(),
+    }
+  }
+
+  pub fn polar(power: f64) -> impl Fn(Complex) -> Complex {
+    move |z: Complex| {
+      let r = z.abs_sq().sqrt();
+      let theta = z.im.atan2(z.re);
+      let r_pow = r.powf(power);
+      let angle = power * theta;
+      Complex {
+        re: r_pow * angle.cos(),
+        im: r_pow * angle.sin(),
+      }
+    }
+  }
+}
