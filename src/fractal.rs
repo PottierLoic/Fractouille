55f554050b4d8c994345d02fae0749cb5ec2354a
Loïc Pottier <loic.pottier@live.fr>
2025-08-04 10:17:24 -0700

    feat: add phoenix set

--- src/fractal.rs
+++ src/fractal.rs
@@ -43,7 +43,7 @@ impl Default for Fractal {
       current_palette: 0,
       set: Set::Mandelbrot,
       julia_constant: Complex::new(-0.5251993, -0.5251993),
-      phoenix_constant:  Complex::new(-0.5251993, -0.5251993),
+      phoenix_constant:  Complex::new( -0.25, 0.0),
       power: 2.0,
     }
   }
