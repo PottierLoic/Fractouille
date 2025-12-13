e8db5514f8df607541062577ff3897e3a94fdb77
PottierLoic <loic.pottier@live.fr>
2025-12-13 09:40:37 +0100

    feat: add tile_rendering WIP

--- src/fractal/render.rs
+++ src/fractal/render.rs
@@ -8,6 +8,17 @@ use image::Rgb;
 use rayon::iter::IntoParallelIterator;
 use rayon::iter::ParallelIterator;
 
+const TOP_TILE: u32 = 64;
+const MIN_TILE: u32 = 8;
+
+#[derive(Clone, Copy)]
+struct Tile {
+  x0: u32,
+  y0: u32,
+  w: u32,
+  h: u32,
+}
+
 impl Fractal {
   pub fn render_frame(&self, width: u32, height: u32, smooth: bool) -> Vec<Vec<Rgb<u8>>> {
     let aspect = width as f64 / height as f64;
@@ -16,51 +27,43 @@ impl Fractal {
     let left = self.z.re - vw / 2.0;
     let top = self.z.im - vh / 2.0;
 
-    (0..height)
-      .into_par_iter()
-      .map(|y| {
-        (0..width)
-          .map(|x| {
-            let cx = left + x as f64 * vw / width as f64;
-            let cy = top + y as f64 * vh / height as f64;
+    let size = width as usize * height as usize;
 
-            let (z0, c0) = match self.set {
-              Set::Mandelbrot | Set::BurningShip => (Complex::new(0.0, 0.0), Complex::new(cx, cy)),
-              Set::Julia => (Complex::new(cx, cy), self.julia_c),
-              Set::Phoenix => (Complex::new(0.0, 0.0), Complex::new(cy, cx)),
-            };
+    let mut tiles = Vec::new();
+    for y in (0..height).step_by(TOP_TILE as usize) {
+      for x in (0..width).step_by(TOP_TILE as usize) {
+        tiles.push(Tile {
+          x0: x,
+          y0: y,
+          w: TOP_TILE.min(width - x),
+          h: TOP_TILE.min(height - y),
+        });
+      }
+    }
 
-            let (iter, final_z) = match self.set {
-              Set::Mandelbrot => {
-                if self.power == 2.0 && in_bulb(c0) {
-                  (255, z0)
-                } else {
-                  iterate_mandelbrot(z0, c0, self.max_iterations, self.power)
-                }
-              }
-              Set::Julia => iterate_julia(z0, c0, self.max_iterations, self.power),
-              Set::BurningShip => iterate_burningship(z0, c0, self.max_iterations),
-              Set::Phoenix => {
-                iterate_phoenix(z0, c0, self.phoenix_p, self.max_iterations, self.power)
-              }
-            };
+    let results: Vec<(Tile, Vec<Rgb<u8>>)> = tiles
+        .into_par_iter()
+        .map(|tile| {
+          let tile_data = process_tile(tile, width, height, left, top, vw, vh, smooth, self);
+          (tile, tile_data)
+        })
+        .collect();
 
-            let value = if smooth && iter < self.max_iterations {
-              let log_zn = final_z.abs_sq().sqrt().ln().ln();
-              iter as f64 + SMOOTH_OFFSET - log_zn / LOG2
-            } else {
-              iter as f64
-            };
+    let mut out = vec![Rgb([0, 0, 0]); size];
 
-            // Return RGB
-            self.colorize(value)
-          })
-          .collect()
-      })
-      .collect()
+    for (tile, data) in results {
+      let mut data_iter = data.into_iter();
+      for y in tile.y0..(tile.y0 + tile.h) {
+        for x in tile.x0..(tile.x0 + tile.w) {
+          out[idx(width, x, y)] = data_iter.next().unwrap();
+        }
+      }
+    }
+
+    out.chunks(width as usize).map(|row| row.to_vec()).collect()
   }
 
-  fn colorize(&self, iter: f64) -> Rgb<u8> {
+  pub fn colorize(&self, iter: f64) -> Rgb<u8> {
     if iter >= self.max_iterations as f64 {
       return Rgb([0, 0, 0]);
     }
@@ -71,8 +74,6 @@ impl Fractal {
   }
 }
 
-// Check if the point is in the main cardioid or period 2 bulb
-// work only for power=2 mandelbrot set
 #[inline(always)]
 fn in_bulb(c: Complex) -> bool {
   let x = c.re - 0.25;
@@ -88,4 +89,162 @@ fn in_bulb(c: Complex) -> bool {
   }
 
   false
+}
+
+#[inline(always)]
+fn compute(
+  x: u32,
+  y: u32,
+  width: u32,
+  height: u32,
+  left: f64,
+  top: f64,
+  vw: f64,
+  vh: f64,
+  smooth: bool,
+  fractal: &Fractal,
+) -> Rgb<u8> {
+  let cx = left + x as f64 * vw / width as f64;
+  let cy = top + y as f64 * vh / height as f64;
+  let c0 = Complex::new(cx, cy);
+  let z0 = Complex::new(0.0, 0.0);
+
+  let (iter, final_z) = match fractal.set {
+    Set::Mandelbrot => {
+      if fractal.power == 2.0 && in_bulb(c0) {
+        (255, z0)
+      } else {
+        iterate_mandelbrot(z0, c0, fractal.max_iterations, fractal.power)
+      }
+    }
+    Set::Julia => iterate_julia(z0, c0, fractal.max_iterations, fractal.power),
+    Set::BurningShip => iterate_burningship(z0, c0, fractal.max_iterations),
+    Set::Phoenix => iterate_phoenix(
+      z0,
+      c0,
+      fractal.phoenix_p,
+      fractal.max_iterations,
+      fractal.power,
+    ),
+  };
+
+  let value = if smooth && iter < fractal.max_iterations {
+    let log_zn = final_z.abs_sq().sqrt().ln().ln();
+    iter as f64 + SMOOTH_OFFSET - log_zn / LOG2
+  } else {
+    iter as f64
+  };
+
+  let color = fractal.colorize(value);
+  color
+}
+
+#[inline(always)]
+fn idx(width: u32, x: u32, y: u32) -> usize {
+  (y as usize) * (width as usize) + (x as usize)
+}
+
+#[inline(always)]
+fn local_idx(x: u32, y: u32, tile_w: u32, tile_x0: u32, tile_y0: u32) -> usize {
+  ((y - tile_y0) * tile_w + (x - tile_x0)) as usize
+}
+
+fn process_tile(
+  tile: Tile,
+  width: u32,
+  height: u32,
+  left: f64,
+  top: f64,
+  vw: f64,
+  vh: f64,
+  smooth: bool,
+  fractal: &Fractal,
+) -> Vec<Rgb<u8>> {
+  let mut tile_pixels = vec![Rgb([0, 0, 0]); (tile.w * tile.h) as usize];
+  let mut calculated_local = vec![false; (tile.w * tile.h) as usize];
+
+  let mut stack = vec![tile];
+  let lid = |x: u32, y: u32| -> usize {
+    local_idx(x, y, tile.w, tile.x0, tile.y0)
+  };
+
+  while let Some(current_tile) = stack.pop() {
+    let mut check_and_compute = |x: u32, y: u32| -> Rgb<u8> {
+      let index = lid(x, y);
+
+      if calculated_local[index] {
+        return tile_pixels[index];
+      }
+
+      let val = compute(x, y, width, height, left, top, vw, vh, smooth, fractal);
+      tile_pixels[index] = val;
+      calculated_local[index] = true;
+      val
+    };
+
+    let x1 = current_tile.x0 + current_tile.w - 1;
+    let y1 = current_tile.y0 + current_tile.h - 1;
+
+    if current_tile.w <= MIN_TILE || current_tile.h <= MIN_TILE {
+      for x in current_tile.x0..=x1 {
+        for y in current_tile.y0..=y1 {
+          check_and_compute(x, y);
+        }
+      }
+      continue;
+    }
+
+    let mut is_interior = true;
+    for x in current_tile.x0..=x1 {
+      let val = check_and_compute(x, current_tile.y0);
+      if val != Rgb([0, 0, 0]) {
+        is_interior = false;
+      }
+      if y1 != current_tile.y0 {
+        let val = check_and_compute(x, y1);
+        if val != Rgb([0, 0, 0]) {
+          is_interior = false;
+        }
+      }
+    }
+
+    for y in (current_tile.y0 + 1)..y1 {
+      let val = check_and_compute(current_tile.x0, y);
+      if val != Rgb([0, 0, 0]) {
+        is_interior = false;
+      }
+      if x1 != current_tile.x0 {
+        let val = check_and_compute(x1, y);
+        if val != Rgb([0, 0, 0]) {
+          is_interior = false;
+        }
+      }
+    }
+
+
+    if is_interior {
+      for x in (current_tile.x0 + 1)..(current_tile.x0 + current_tile.w - 1) {
+        for y in (current_tile.y0 + 1)..(current_tile.y0 + current_tile.h - 1) {
+          let index = lid(x, y);
+          tile_pixels[index] = Rgb([255, 255, 255]);
+          calculated_local[index] = true;
+        }
+      }
+    } else {
+      let hw = current_tile.w / 2;
+      let hh = current_tile.h / 2;
+      let w0 = hw;
+      let w1 = current_tile.w - hw;
+      let h0 = hh;
+      let h1 = current_tile.h - hh;
+
+      stack.push(Tile { x0: current_tile.x0, y0: current_tile.y0, w: w0, h: h0, });
+      stack.push(Tile { x0: current_tile.x0 + w0, y0: current_tile.y0, w: w1, h: h0, });
+      stack.push(Tile { x0: current_tile.x0, y0: current_tile.y0 + h0, w: w0, h: h1, });
+      stack.push(Tile { x0: current_tile.x0 + w0, y0: current_tile.y0 + h0, w: w1, h: h1, });
+    }
+  }
+
+
+  tile_pixels
 }
